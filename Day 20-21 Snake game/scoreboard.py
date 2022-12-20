a59c6f02c910f278adf8af31d0ab90fb86392a0f
TwigaX <j.k.reszka@gmail.com>
2022-12-20 13:56:19 +0100

    Add detect collision with wall and game over message

--- Day 20-21 Snake game/scoreboard.py
+++ Day 20-21 Snake game/scoreboard.py
@@ -15,7 +15,13 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align = ALIGNMENT, font = FONT )
 
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align = ALIGNMENT, font = FONT)
+
     def count_score(self):
         self.score +=1
         self.clear()
         self.update_scoreboard()
+
+
--- Day 20-21 Snake game/snake_game.py
+++ Day 20-21 Snake game/snake_game.py
@@ -32,6 +32,11 @@ while game_on:
         food.refresh()
         scoreboard.count_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
 
 
 
