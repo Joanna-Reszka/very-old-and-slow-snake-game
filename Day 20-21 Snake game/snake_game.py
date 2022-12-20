4b1e9fbcaad467217d274f4fda478f56ef5bd86b
TwigaX <j.k.reszka@gmail.com>
2022-12-20 13:23:45 +0100

    Add food class and collision snake- food

--- Day 20-21 Snake game/snake_game.py
+++ Day 20-21 Snake game/snake_game.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
@@ -9,6 +10,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, 'Up')
 screen.onkey(snake.down, 'Down')
@@ -18,8 +21,13 @@ screen.onkey(snake.right, 'Right')
 game_on = True
 while game_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.25)
     snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15: #dist fro  head of snake to food less than 15 pixels
+        food.refresh()
+
 
 screen.exitonclick()
 
