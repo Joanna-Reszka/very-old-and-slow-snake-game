0cbb889cfd8d889ea16a2bb7b05332500126eb75
TwigaX <j.k.reszka@gmail.com>
2022-12-21 17:16:29 +0100

    Create Scoreboard and keep scores

--- Day 22 Pong/ball.py
+++ Day 22 Pong/ball.py
@@ -7,9 +7,9 @@ class Ball(Turtle):
         self.shape('circle')
         self.color("white")
         self.penup()
-        self.goto(0,0)
         self.x_step =10
         self.y_step = 10
+        self.move_speed = 0.1
 
     def move(self):
         #self.setheading(45)
@@ -17,5 +17,13 @@ class Ball(Turtle):
         new_y = self.ycor() + self.y_step
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_step *= -1
+        self.move_speed *= 0.9
+    def bounce_x(self):
+        self.x_step *= -1
+        self.move_speed *= 0.9
+    def reset(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- Day 22 Pong/pong.py
+++ Day 22 Pong/pong.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from score import Scoreboard
 import time
 
 screen = Screen()
@@ -9,29 +10,40 @@ screen.bgcolor('black')
 screen.title("Pong Game")
 screen.tracer(0)
 ball = Ball()
+score = Scoreboard()
 r_paddle = Paddle(350,0)
 l_paddle = Paddle(-350,0)
 screen.listen()
 
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
-    time.sleep(0.05)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     # Detect collision with upper or lower wall
-    if ball.ycor() > 285 or ball.ycor() < -285:  # dist fro  wall less than 30 pixels
-        ball.bounce()
+    if ball.ycor() > 285 or ball.ycor() < -285:  # dist from  wall less than 30 pixels
+        ball.bounce_y()
 
-    # Detect collision side walls
-    if ball.xcor() > 400 or ball.xcor() < -400:
-        game_on = False
-        #scoreboard.game_over()
+    # Detect collision with right wall = right paddle misses
+    if ball.xcor() > 400:
+        ball.reset()
+        score.l_point()
+    # Detect collision left wall = left paddle misses
+    if ball.xcor() < -400:
+        ball.reset()
+        score.r_point()
+    #ball.move()
+    #game_on = False
+    #scoreboard.game_over()
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor()> 330 or ball.distance(l_paddle) < 50 and ball.xcor()< -330:
+        ball.bounce_x()
 
 
 screen.exitonclick()
